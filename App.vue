df621447de4a54a01becde665e9fcfb0c7c3c654
antan <154360385+antan1011@users.noreply.github.com>
2023-12-31 20:53:17 +0800

    Update App.vue

--- App.vue
+++ App.vue
@@ -1,7 +1,10 @@
 <template>
-    <layout></layout>
+    <v-md-editor v-model="text" :left-toolbar="toolbar" :disabled-menus="[]" height="500px"/>
 </template>
 
 <script setup>
-import Layout from './components/Layout.vue'
+    import { ref } from 'vue'
+
+    const toolbar = 'undo redo clear | h bold italic strikethrough quote | ul ol table hr | link image code | save'
+    const content = ref('It was nice to meet you!')
 </script>
